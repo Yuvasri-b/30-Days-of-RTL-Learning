0bf3105a2c711074e781261a3f1ecfe2ec4002a2
YUVA SRI B <155957336+Yuvasri-b@users.noreply.github.com>
2024-04-03 23:34:29 +0530

    binary_gray_converter

---
+++ Day_19/binary_gray_converter.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+
+module binary_to_gray(bin,gray);
+
+input[3:0]bin; 
+output[3:0]gray; 
+
+assign gray[3]=bin[3];
+assign gray[2]=bin[3]^bin[2]; 
+assign gray[1]=bin[2]^bin[1]; 
+assign gray[0]=bin[1]^bin[0]; 
+
+endmodule
